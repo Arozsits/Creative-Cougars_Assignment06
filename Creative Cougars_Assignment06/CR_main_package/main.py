0f9231d0dcc1878b80b47d1879e46ae974e4b386
cmramsey3 <coltonramsey3@gmail.com>
2025-02-20 14:02:51 -0500

    added program documentation to main.py

--- Creative Cougars_Assignment06/CR_main_package/main.py
+++ Creative Cougars_Assignment06/CR_main_package/main.py
@@ -1,1 +1,14 @@
+# File Name : main.py
+# Student Name: Colton Ramsey
+# email: ramseyc6@mail.uc.edu
+# Assignment Number: Assignment 06
+# Due Date: 02/27/2025
+# Course #/Section: IS 4010-001
+# Semester/Year: Spring 2025
+# Brief Description of the assignment: Collaborate with a group to create a python solution that contains 3 different classes
+# revolving around the same topic and a main that is used to call and insatiate all of the classes
+
+# Brief Description of what this module does: The main will call and instatiate the classes created by the other group members
+# Citations: None
+# Anything else that's relevant: None
 
